7ae2b3dd06009372c6c405d306c747a0424ceb05
Wesley Peter <wpeter@salesforce.com>
2019-04-22 11:10:03 -0700

    output es module, set some env variables

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,17 @@ import fs from "fs-extra";
 import path from "path";
 import { terser } from "rollup-plugin-terser";
 import serve from "rollup-plugin-serve";
+import alias from "rollup-plugin-alias";
+const resolve = require("rollup-plugin-node-resolve");
 
+const projectPackage = require("./package.json");
 const distFolderName = "dist";
 const dist = path.resolve(__dirname, `./${distFolderName}`);
 const publicFolder = path.resolve(__dirname, `./public`);
 const isWatching = process.env.ROLLUP_WATCH;
+
+const environment = process.env.build || "development";
+const isProduction = environment === "production";
 
 // Start with clean dist folder
 fs.removeSync(dist);
@@ -17,19 +23,34 @@ fs.removeSync(dist);
 // Copy public folder
 fs.copySync(publicFolder, dist);
 
-const env = process.env.build;
-
 export default {
     input: "no-types/index.js",
     output: {
         file: `${distFolderName}/bundle.js`,
-        format: "iife",
-        name: "Main"
+        format: "esm"
     },
     plugins: [
-        lwcCompiler(),
-        replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
-        env !== "dev" && terser({ sourcemap: false }),
+        lwcCompiler({
+            stylesheetConfig: { customProperties: { allowDefinition: true } },
+            resolveFromPackages: false
+        }),
+        alias({
+            lwc: require.resolve("@lwc/engine/dist/modules/es2017/engine.js")
+        }),
+        resolve({
+            modulesOnly: true
+        }),
+        replace({
+            "process.env.NODE_ENV": JSON.stringify(environment),
+            "process.env.ENVIRONMENT": JSON.stringify(environment),
+            "process.env.RELEASE_VERSION": JSON.stringify(
+                projectPackage.version
+            ),
+            "process.env.RELEASE_DATE": JSON.stringify(
+                new Date().toLocaleDateString("en-US")
+            )
+        }),
+        isProduction && terser({ sourcemap: false }),
         isWatching &&
             serve({
                 contentBase: dist,
