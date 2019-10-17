8867f8aae7192dff2ea550030ad38d0730f60fb6
Wesley Peter <wpeter@salesforce.com>
2019-10-17 11:19:08 -0700

    import from graphql

--- src/modules/template/balloon/balloon.ts
+++ src/modules/template/balloon/balloon.ts
@@ -1,6 +1,11 @@
 import { LightningElement, api } from "lwc";
+import { version } from "graphql";
 
 export default class Balloon extends LightningElement {
+    connectedCallback() {
+        console.log(`using graphql version ${version}`);
+    }
+
     @api
     public testProp: any;
 }
